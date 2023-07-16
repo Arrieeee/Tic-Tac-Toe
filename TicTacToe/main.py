b42c27f42647576fb87da4487d4a9c79f9aebbb3
Arno Mostert <94126075+Arrieeee@users.noreply.github.com>
2023-07-16 23:41:52 +0200

    Update main.py

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,31 +1,12 @@
+'''
+-time: 2023/07/16 23:41
+-author: https://github.com/Arrieeee
+'''
+
+
 from GameFiles import GameLogic
-import os
-import neat
 
-
-'''def eval_genomes(genomes, config):
-    width, height = 700, 500
-
-def run_neat(config):
-    #p = neat.Checkpointer.restore_checkpoint('')
-
-    p = neat.Population(config)
-    p.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    p.add_reporter(stats)
-    p.add_reporter(neat.Checkpointer(1))
-
-    winner = p.run(eval_genomes, 50)'''
 
 if __name__ == '__main__':
-    '''local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, "config.txt")
-
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
-                         config_path)
-    
-    run_neat(config)'''
-
     game = GameLogic()
     game.run()
